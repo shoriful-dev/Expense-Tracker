a0b1ee4426ca48dd6f46878b654c5a718e2d3e9b
shoriful-dev <cmshohelrana50@gmail.com>
2026-04-02 08:49:22 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Routes } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+
+      </Routes>
+    </>
+  );
+};
+
+export default App;
