dbc47e394d6eeeb82df9b77e6b845ea37611d627
shoriful-dev <cmshohelrana50@gmail.com>
2026-04-03 19:52:55 +0600

    create sitebar

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { styles } from '../assets/dummyStyles';
 import Navber from './Navber';
+import Sidebar from './Sidebar';
 
 const Layout = ({ onLogout, user }) => {
+  const [sidebarCollapsed, setsidebarCollapsed] = useState(false);
   return (
     <div className={styles.layout.root}>
       <Navber user={user} onLogout={onLogout}/>
+      <Sidebar user={user} isCollapsed={sidebarCollapsed} setIsCollapsed={setsidebarCollapsed}/>
     </div>
   );
 };
